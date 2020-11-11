ee64dfe4069cd8a62ac8c2cf45e126033a35511b
lanceLot-rv <1065842336@qq.com>
2020-11-12 00:09:24 +0800

    11111

--- cloud-provider-payment8001/src/main/java/com/lsc/springcloud/config/RedisConfig.java
+++ cloud-provider-payment8001/src/main/java/com/lsc/springcloud/config/RedisConfig.java
@@ -2,10 +2,13 @@ package com.lsc.springcloud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.scripting.support.ResourceScriptSource;
 
 import java.io.Serializable;
 
@@ -19,5 +22,13 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
+    }
+
+    @Bean
+    public DefaultRedisScript<String> defaultRedisScript() {
+        DefaultRedisScript<String> defaultRedisScript = new DefaultRedisScript<>();
+        defaultRedisScript.setResultType(String.class);
+        defaultRedisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("lua/demo.lua")));
+        return defaultRedisScript;
     }
 }
--- cloud-provider-payment8001/src/main/java/com/lsc/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/lsc/springcloud/controller/PaymentController.java
@@ -4,12 +4,17 @@ import com.lsc.springcloud.entities.CommonResult;
 import com.lsc.springcloud.service.ConfigTestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.ReturnType;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController(value = "payment")
@@ -20,6 +25,9 @@ public class PaymentController {
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    DefaultRedisScript<String> redisScript;
 
     @PostMapping(value = "create")
     public CommonResult create() {
@@ -40,6 +48,25 @@ public class PaymentController {
 
     @GetMapping("redis")
     public String redis(){
+        String key=String.valueOf("goods");
+        String value = UUID.randomUUID().toString();
+        boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, value, 5L, TimeUnit.SECONDS);
+
+        String args="22222"+"-"+System.currentTimeMillis();
+        Object res = stringRedisTemplate.execute((RedisConnection connection) -> connection.eval(
+                redisScript.getScriptAsString().getBytes(),
+                ReturnType.INTEGER,
+                1,
+                key.getBytes(),
+                args.getBytes()));
+        Long result= (Long) res;
+        if(result==1){
+            String unitAmountStr = (String) stringRedisTemplate.opsForHash().get("red_packet_" + key,"unit_amount");
+            Double unitAmount = Double.valueOf(unitAmountStr);
+            stringRedisTemplate.delete(key);
+        }
+
+
         return stringRedisTemplate.opsForValue().get("goods");
     }
 }
